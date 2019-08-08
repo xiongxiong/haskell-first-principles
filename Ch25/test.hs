ba632f824912163cf0d5016d77549cee79c612d6
wonderbear <ximengwuheng@163.com>
2019-08-08 19:54:09 +0800

    update

--- Ch25/test.hs
+++ Ch25/test.hs
@@ -1,12 +1,21 @@
-newtype Identity a = Identity { runIdentity :: a }
+{-# LANGUAGE InstanceSigs #-}
 
-newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
+newtype Identity a = Identity { runIdentity :: a }
 
 instance Functor Identity where
     fmap f (Identity a) = Identity (f a)
 
+newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
+
 instance (Functor f, Functor g) => Functor (Compose f g) where 
     fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where 
+    pure :: a -> Compose f g a
+    pure a = Compose . pure . pure $ a
+
+    (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+    (Compose f) <*> (Compose a) = Compose 
 
 newtype One f a = One (f a) deriving (Eq, Show)
 
