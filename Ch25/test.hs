772db41e1c030d0d33d93adbfa37af7c38ee3703
wonderbear <ximengwuheng@163.com>
2019-08-08 19:03:12 +0800

    update

--- Ch25/test.hs
+++ Ch25/test.hs
@@ -7,3 +7,15 @@ instance Functor Identity where
 
 instance (Functor f, Functor g) => Functor (Compose f g) where 
     fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+newtype One f a = One (f a) deriving (Eq, Show)
+
+instance Functor f => Functor (One f) where 
+    fmap f (One fa) = One $ fmap f fa
+
+newtype Three f g h a = Three (f (g (h a))) deriving (Eq, Show)
+
+instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where 
+    fmap f (Three fgha) = Three $ (fmap . fmap . fmap) f fgha
+
+
