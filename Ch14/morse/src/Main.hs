a9614630a93276a767908553b6d95d6cc1c886fd
wonderbear <ximengwuheng@163.com>
2019-09-24 18:54:34 +0800

    modified

--- Ch14/morse/src/Main.hs
+++ Ch14/morse/src/Main.hs
@@ -10,7 +10,22 @@ import System.IO (hGetLine, hIsEOF, stdin)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    mode <- getArgs 
+    case mode of 
+        [arg] -> 
+            case arg of 
+                "from" -> convertFromMorse 
+                "to" -> convertToMorse 
+                _ -> argError
+        _ -> argError
+
+    where argError = do 
+        putStrLn "Please specify the \
+            \ first argument \
+            \ as being 'from' or\
+            \ 'to' morse,\
+            \ such as: morse to"
+        exitFailure 
 
 convertToMorse :: IO ()
 convertToMorse = forever $ do
@@ -28,4 +43,26 @@ convertToMorse = forever $ do
           -> putStrLn (intercalate " " str)
         Nothing
           -> do
-            putStrLn $ "ERROR:"
+            putStrLn $ "ERROR: " ++ line
+            exitFailure
+
+convertFromMorse :: IO ()
+convertFromMorse = forever $ do
+    weAreDone <- hIsEOF stdin 
+    when weAreDone exitSuccess 
+
+    line <- hGetLine stdin 
+    convertLine line 
+
+    where 
+        convertLine line = do 
+            let decoded :: Maybe String
+                decoded = 
+                    traverse morseToChar (words line)
+
+            case decoded of
+                (Just s) -> putStrLn s
+                Nothing -> do 
+                    putStrLn $ "ERROR: " ++ line 
+                    exitFailure 
+
