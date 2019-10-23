6575bd5ad2265ba29ec6b4f86267bf2eef959b4a
wonderbear <ximengwuheng@163.com>
2019-10-23 20:03:26 +0800

    update

--- Ch16/Exercise16/src/Main.hs
+++ Ch16/Exercise16/src/Main.hs
@@ -10,3 +10,10 @@ instance Functor FixMePls where
   fmap _ FixMe = FixMe
   fmap f (Pls a) = Pls (f a)
 
+--
+
+data CountingBad a =
+  Heisenberg Int a deriving (Eq, Show)
+
+instance Functor CountingBad where
+  fmap f (Heisenberg n a) = Heisenberg (n+1) (f a)
