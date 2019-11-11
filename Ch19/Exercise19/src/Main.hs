89a51226fd6fbf39654f0f2aca34969533423897
xiongxiong <ximengwuheng@163.com>
2019-11-12 07:42:08 +0800

    update

--- Ch19/Exercise19/src/Main.hs
+++ Ch19/Exercise19/src/Main.hs
@@ -3,7 +3,10 @@
 module Main where
 
 import Data.Monoid (mconcat)
-import Web.Scotty
+import Web.Scotty (scotty, get, html)
+import Database.Persist.Sql hiding (get)
+import Data.Conduit
+import Data.Conduit.Lift
 
 main :: IO ()
 main = scotty 3000 $
@@ -11,6 +14,11 @@ main = scotty 3000 $
     beam <- param "word"
     html $ mconcat [
         "<h1>Scotty, ",
-        "beam",
+        beam,
         " me up!</h1>"
       ]
+
+runDb :: SqlPersist (ResourceT IO) a -> IO a
+runDb query = do
+  let connStr = foldr (\(k,v) t -> t <> (encodeUtf8 $ k <> "=" <> v <> " ")) "" params
+  runResourceT . withPostgresqlConn connStr $ runSqlConn query
