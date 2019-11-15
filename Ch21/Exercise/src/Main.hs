1bec5ba94c4b440c7389222be159e972352617a5
wonderbear <ximengwuheng@163.com>
2019-11-15 18:19:39 +0800

    update

--- Ch21/Exercise/src/Main.hs
+++ Ch21/Exercise/src/Main.hs
@@ -1,5 +1,37 @@
 module Main where
 
+import Data.Traversable
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+data Query = Query
+data SomeObj = SomeObj
+data IoOnlyObj = IoOnlyObj
+data Err = Err
+
+decodeFn :: String -> Either Err SomeObj
+decodeFn = undefined
+
+fetchFn :: Query -> IO [String]
+fetchFn = undefined
+
+makeIoOnlyObj :: [SomeObj] -> IO [(SomeObj, IoOnlyObj)]
+makeIoOnlyObj = undefined
+
+pipelineFn :: Query -> IO (Either Err [(SomeObj, IoOnlyObj)])
+-- pipelineFn query = do
+--   a <- fetchFn query
+--   case sequence (map decodeFn a) of
+--     (Left err) -> return $ Left $ err
+--     (Right res) -> do
+--       a <- makeIoOnlyObj res
+--       return $ Right a
+-- pipelineFn query = do
+--   a <- fetchFn query
+--   traverse makeIoOnlyObj (mapM decodeFn a)
+-- pipelineFn = 
+--   (traverse makeIoOnlyObj . mapM decodeFn =<<) . fetchFn
+pipelineFn =
+  (traverse makeIoOnlyObj . traverse decodeFn =<<) . fetchFn 
